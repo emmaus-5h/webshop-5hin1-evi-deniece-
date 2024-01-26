73e2be35bd394f5d37a9fa5d6c5511ccdbfbad0b
DenieceWestphal <115075303+DenieceWestphal@users.noreply.github.com>
2024-01-26 14:17:48 +0000

    prijzen en tekst

--- db/create.sql
+++ db/create.sql
@@ -21,9 +21,9 @@ CREATE TABLE products (
 --
 
 insert into products (name, description, code, price) values ('Cock Ring', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 14,99);
-insert into products (name, description, code, price) values ('Buttplug', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Analbeads', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Vibrator', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Rose Toy', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Penis Pump', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('Buttplug', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 12,50);
+insert into products (name, description, code, price) values ('Analbeads', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 8,99);
+insert into products (name, description, code, price) values ('Vibrator', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 17,99);
+insert into products (name, description, code, price) values ('Rose Toy', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14,99);
+insert into products (name, description, code, price) values ('Cock Ring', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 21,99);
 
