09573042a4fa06d1d89dd2fca62800fa63e36c0d
DenieceWestphal <115075303+DenieceWestphal@users.noreply.github.com>
2024-02-02 14:29:57 +0000

    nieuwe rijen

--- db/create.sql
+++ db/create.sql
@@ -22,8 +22,11 @@ CREATE TABLE products (
 
 insert into products (name, description, code, price) values ('Buttplug', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 14.99);
 insert into products (name, description, code, price) values ('Analbeads', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 12.50);
-insert into products (name, description, code, price) values ('Vibrator', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 8.99);
+insert into products (name, description, code, price) values ('Vibrator', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 12.00);
 insert into products (name, description, code, price) values ('Rose Toy', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 17.99);
 insert into products (name, description, code, price) values ('Cock Ring', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14.99);
-insert into products (name, description, code, price) values ('Mini Wand Vibrator', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 21.99);
+insert into products (name, description, code, price) values ('Mini Wand Vibrator', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 8.99);
+insert into products (name, description, code, price) values ('Fleshlight', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 21.99);
+insert into products (name, description, code, price) values ('Hema Worst', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 21.99);
+insert into products (name, description, code, price) values ('Tong Vibrator', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 21.99);
 
