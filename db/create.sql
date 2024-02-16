69f173d9835468e0737012138bae8c019f6d093f
DenieceWestphal <115075303+DenieceWestphal@users.noreply.github.com>
2024-02-16 09:47:13 +0000

    tabellen

--- db/create.sql
+++ db/create.sql
@@ -43,27 +43,32 @@ insert into colors
 insert into products (name, description, code, price) values ('Buttplug', 'Een stainless steel buttplug gemaakt voor genot en plezier voor de anus. Versierd met een roze diamant voor de ultieme sensatie.', '816905633-0', 14.99);
 insert into products (name, description, code, price) values ('Analbeads', 'Een silicone langwerpige analbeads staaf gemaakt voor genot en plezier voor de anus.', '077030122-3', 12.50);
 insert into products (name, description, code, price) values ('Vibrator', 'Vibrerende toy voor het vrouwelijke geslachtsdeel ', '445924201-X', 12.00);
-insert into products (name, description, code, price) values ('Rose Toy', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 17.99);
-insert into products (name, description, code, price) values ('Cock Ring', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14.99);
-insert into products (name, description, code, price) values ('Mini Wand Vibrator', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 8.99);
-insert into products (name, description, code, price) values ('Fleshlight', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 18.50);
-insert into products (name, description, code, price) values ('Hema Worst', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 19.99);
-insert into products (name, description, code, price) values ('Tong Vibrator', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14.99);
-insert into products (name, description, code, price) values ('Love Package', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 24.99);
-insert into products (name, description, code, price) values ('Condooms', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 6.99);
-insert into products (name, description, code, price) values ('Satisfyer', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 15.99);
+insert into products (name, description, code, price) values ('Rose Toy', 'vibrerend roos vormig siliconen speeltje.', '693155505-7', 17.99);
+insert into products (name, description, code, price) values ('Cock Ring', 'siliconen ring voor om het mannelijke geslachtsdeel, voor genot en plezier.', '686928463-6', 14.99);
+insert into products (name, description, code, price) values ('Mini Wand Vibrator', 'mini vibrator, perfect gemaaktvoor op reis.', '492662523-7', 8.99);
+insert into products (name, description, code, price) values ('Fleshlight','metalen omhulzing gevuld met siliconen, voor mannelijk gebruik.' , '492662523-7', 18.50);
+insert into products (name, description, code, price) values ('Hema Worst', 'Altijd al fan van de Hema geweest? Dan is dit het perfecte speeltje voor jou!', '492662523-7', 19.99);
+insert into products (name, description, code, price) values ('Tong Vibrator', 'vibrerend speeltje speciaal gemaakt voor stimulatie voor de clitoris.', '492662523-7', 14.99);
+insert into products (name, description, code, price) values ('Satisfyer', 'Perfect speeltje voor het vrouwelijke geslachtsdeel. Voor een goedkope prijs.', '492662523-7', 15.99);
 
 
 insert into colors (colorsNAME) values ('roze');
 insert into colors (colorsNAME) values ('zwart');
 insert into colors (colorsNAME) values ('zilver);
-insert into colors (colorsNAME) values ('licht roze');
-insert into colors (colorsNAME) values ('paars');
 
 
 insert into sortBYcolor (products_id, color_id) values (1,2);
-insert into sortBYcolor (products_id, color_id) values (1,2);
-insert into sortBYcolor (products_id, color_id) values (1,2);
-insert into sortBYcolor (products_id, color_id) values (1,2);
-insert into sortBYcolor (products_id, color_id) values (1,2);
-  insert into sortBYcolor (products_id, color_id) values (1,2);
+insert into sortBYcolor (products_id, color_id) values (1,3);
+insert into sortBYcolor (products_id, color_id) values (2,2);
+insert into sortBYcolor (products_id, color_id) values (3,3);
+insert into sortBYcolor (products_id, color_id) values (3,5);
+insert into sortBYcolor (products_id, color_id) values (4,2);
+insert into sortBYcolor (products_id, color_id) values (5,1);
+insert into sortBYcolor (products_id, color_id) values (5,2);
+  insert into sortBYcolor (products_id, color_id) values (6,8); 
+
+  insert into populariteit (hoeveelheid_populariteit, reden_populariteit) values ('niet populair', 'werkt niet goed');
+  insert into populariteit (hoeveelheid_populariteit, reden_populariteit) values ('redelijk populair', 'wordt verkocht');
+  insert into populariteit (hoeveelheid_populariteit, reden_populariteit) values ('populair', 'Staat in de top 10');
+  insert into populariteit (hoeveelheid_populariteit, reden_populariteit) values ('erg populair', 'wordt veel verkocht en aangeraden');
+  
