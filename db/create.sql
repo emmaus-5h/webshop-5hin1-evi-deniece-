f8ad08bda444731c78a606129e71449e218cb49e
DenieceWestphal <115075303+DenieceWestphal@users.noreply.github.com>
2024-02-18 15:26:54 +0000

    proberen de code te laten werken

--- db/create.sql
+++ db/create.sql
@@ -3,15 +3,20 @@
 --
 
 CREATE TABLE products (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  code VARCHAR(15),
-  name VARCHAR(255),
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  code numeric
+  name TEXT,
   description TEXT,
-  price NUMERIC(10, 2)
+  price NUMERIC,
+  sizes_id integer,
+  colors_id integer,
+  catagories_id integer,
+  populariteit_id integer,
+  rate_id integer
 );
 CREATE TABLE sizes (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  maat VARCHAR(20)
+  maat TEXT 
 );
 CREATE TABLE colors (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,12 +25,12 @@ CREATE TABLE colors (
 );
 CREATE TABLE catagories (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  geslacht VARCHAR (10),
+  geslacht TEXT
 );
 CREATE TABLE populariteit (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  hoeveelheid_populariteit VARCHAR (30),
-  reden_populariteit VARCHAR (40)
+  hoeveelheid_populariteit TEXT,
+  reden_populariteit TEXT
 );
 CREATE TABLE rate (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -43,15 +48,24 @@ sterren TEXT,
 --
 
 insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Buttplug', 'Een stainless steel buttplug gemaakt voor genot en plezier voor de anus. Versierd met een roze diamant voor de ultieme sensatie.', '816905633-0', 14.99, 3, 1, 4);
-insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Analbeads', 'Een silicone langwerpige analbeads staaf gemaakt voor genot en plezier voor de anus.', '077030122-3', 12.50,4, 1, 5);
+
+insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Analbeads', 'Een silicone langwerpige analbeads staaf gemaakt voor genot en plezier voor de anus.', '077030122-3', 12.50,4,1,5);
+
 insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Vibrator', 'Vibrerende toy voor het vrouwelijke geslachtsdeel ', '445924201-X', 12.00, 3, 2, 5 );
+
 insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Rose Toy', 'vibrerend roos vormig siliconen speeltje.', '693155505-7', 17.99, 3, 2, 1);
+
 insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Cock Ring', 'siliconen ring voor om het mannelijke geslachtsdeel, voor genot en plezier.', '686928463-6', 14.99, 2, 1, 2);
+
 insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Mini Wand Vibrator', 'mini vibrator, perfect gemaaktvoor op reis.', '492662523-7', 8.99, 1, 2, 5);
+
 insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Fleshlight','metalen omhulzing gevuld met siliconen, voor mannelijk gebruik.' , '492662523-7', 18.50, 2, 1, 5);
-insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Hema Worst', 'Altijd al fan van de Hema geweest? Dan is dit het perfecte speeltje voor jou!', '492662523-7', 19.99,5 , 2,3);
-insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Tong Vibrator', 'vibrerend speeltje speciaal gemaakt voor stimulatie voor de clitoris.', '492662523-7', 14.99, 3, 1,4);
-insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Satisfyer', 'Perfect speeltje voor het vrouwelijke geslachtsdeel. Voor een goedkope prijs.', '492662523-7', 15.99, 2,2, 4);
+
+insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Hema Worst', 'Altijd al fan van de Hema geweest? Dan is dit het perfecte speeltje voor jou!', '492662523-7', 19.99,5,2,3);
+
+insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Tong Vibrator', 'vibrerend speeltje speciaal gemaakt voor stimulatie voor de clitoris.', '492662523-7', 14.99, 3,1,4);
+
+insert into products (name, description, code, price, sizes_id, catogories_id, rate_id) values ('Satisfyer', 'Perfect speeltje voor het vrouwelijke geslachtsdeel. Voor een goedkope prijs.', '492662523-7', 15.99, 2,2,4);
 
 
 insert into colors (products_id, color_id) values (1,2);
@@ -68,7 +82,7 @@ insert into colors (products_id, color_id) values (5,2);
   insert into populariteit (hoeveelheid_populariteit, reden_populariteit) values ('redelijk populair', 'wordt verkocht');
   insert into populariteit (hoeveelheid_populariteit, reden_populariteit) values ('populair', 'Staat in de top 10');
   insert into populariteit (hoeveelheid_populariteit, reden_populariteit) values ('erg populair', 'wordt veel verkocht en aangeraden');
-insert into populariteit (hoeveelheid_populariteit, reden_populariteit) values ('echt heeeeeeeeeeeeeeeeeeel populair echt heeeeeeeeeeeeeeeel erg popu', 'komt niet meer terug want het wordt zoveel verkocht');
+insert into populariteit (hoeveelheid_populariteit, reden_populariteit) values ('echt heel erg populair', 'komt niet meer terug want het wordt zoveel verkocht');
 
 insert into sizes (maat) values ('XS');
 insert into sizes (maat) values ('S');
@@ -83,7 +97,7 @@ insert into rate (sterren) values ('heel slecht');
 insert into rate (sterren) values ('slecht');
 insert into rate (sterren) values ('matig');
 insert into rate (sterren) values ('goed');
-insert into rate (sterren) values ('fucking goed echt heel lekker');
+insert into rate (sterren) values ('heel goed, echt heel lekker');
 
 
 
